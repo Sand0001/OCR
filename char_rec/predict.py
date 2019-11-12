cdd3bf801e79e108aca9b1e4917c2c2f955cb117
fengjing <fengjing@dip-ai.com>
2019-11-12 18:19:37 +0800

    1

--- char_rec/predict.py
+++ char_rec/predict.py
@@ -9,10 +9,10 @@ from char_rec import shufflenet_res_crnn as densenet
 #from char_rec import dl_resnet_crnn_cudnnlstm as resnet
 
 decode_ctc = decode_ctc(eng_dict_path_file='./char_rec/corpus/eng_dict.pkl',
-                      #lfreq_chn_word_path='./char_rec/corpus/char_and_word_bigram_chneng.json',
-                      #lfreq_jap_word_path='./char_rec/corpus/char_and_word_bigram_jap.json')
-                      lfreq_chn_word_path='./char_rec/corpus/count_word_chn0.json',
-                      lfreq_jap_word_path='./char_rec/corpus/count_word_chn0.json')
+                      lfreq_chn_word_path='./char_rec/corpus/char_and_word_bigram_chneng.json',
+                      lfreq_jap_word_path='./char_rec/corpus/char_and_word_bigram_jap.json')
+                      #lfreq_chn_word_path='./char_rec/corpus/count_word_chn0.json',
+                      #lfreq_jap_word_path='./char_rec/corpus/count_word_chn0.json')
 graph = tf.get_default_graph()
 class predict():
     def __init__(self,**kwargs):
